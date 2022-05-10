3cd78eca9272bf5720ce73af200abf5868b73e57
yooseon <103306073+yooseon0611@users.noreply.github.com>
2022-05-10 22:34:16 +0900

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.local.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.local.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://nsfasakhrjgimjgbwylh.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zZmFzYWtocmpnaW1qZ2J3eWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIxMDM0NjksImV4cCI6MTk2NzY3OTQ2OX0.DqWyQeieymeTyARUiYTCLL6yKHOE14Q2HdUxUE8NWcY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
