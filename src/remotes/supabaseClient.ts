dfccce1e1b8436e104c8eaefb627be300c351296
yooseon <103306073+yooseon0611@users.noreply.github.com>
2022-05-10 23:25:34 +0900

    Update supabaseClient.ts

--- src/remotes/supabaseClient.ts
+++ src/remotes/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://nsfasakhrjgimjgbwylh.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zZmFzYWtocmpnaW1qZ2J3eWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIxMDM0NjksImV4cCI6MTk2NzY3OTQ2OX0.DqWyQeieymeTyARUiYTCLL6yKHOE14Q2HdUxUE8NWcY'
 
 if (supabaseUrl == null || supabaseAnonKey == null) {
   throw Error(
